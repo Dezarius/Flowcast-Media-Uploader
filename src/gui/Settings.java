164ba9040f58ce7b3bdddcef999740a68316b81a
Dezarius <abelmannjan@me.com>
2015-11-19 17:08:46 +0100

    Settingsauslesen

--- src/gui/Settings.java
+++ src/gui/Settings.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.util.Arrays;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
@@ -23,6 +24,10 @@ import sun.awt.SunToolkit;
 public class Settings extends JDialog implements ActionListener, DocumentListener, WindowListener{
     
     JDialog popup;
+    
+    private String serverIP = "";
+    private String username = "";
+    private String password = "";
     
     private JTextField tf_server = new JTextField();
     private JTextField tf_user = new JTextField();
@@ -99,7 +104,15 @@ public class Settings extends JDialog implements ActionListener, DocumentListene
     public void actionPerformed(ActionEvent e) {
         Object o = e.getSource();
         if(o == this.b_save) {
+            this.password = String.valueOf(this.tf_password.getPassword());
+            this.serverIP = this.tf_server.getText();
+            this.username = this.tf_user.getText();
             
+            System.out.println(this.serverIP);
+            System.out.println(this.username);
+            System.out.println(this.password);
+            
+            Settings.this.dispose();
         }  
     }
 	
